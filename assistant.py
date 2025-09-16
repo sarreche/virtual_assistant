1cfefce8e3c705108699a73c9e8e4e3201c76041
Santiago Arreche <sarreche@gmail.com>
2025-09-16 09:31:22 -0300

    fix(assistant) : translate instruccions to english

--- assistant.py
+++ assistant.py
@@ -171,43 +171,43 @@ def main_loop():
         # activate microphone and save request as string
         request = transform_audio_to_text().lower()
 
-        if 'abrir youtube' in request:
+        if 'open youtube' in request:
             speak('Sure, opening YouTube')
             webbrowser.open('https://www.youtube.com')
             continue
-        elif 'abrir navegador' in request:
+        elif 'open browser' in request:
             speak('Of course, opening browser')
             webbrowser.open('https://www.google.com')
             continue
-        elif 'qué día es hoy' in request:
+        elif "what day is today" in request:
             get_day()
             continue
-        elif 'qué hora es' in request:
+        elif "what time is it" in request:
             get_time()
             continue
-        elif 'busca en wikipedia' in request:
+        elif 'search wikipedia' in request:
             speak('Searching that on Wikipedia')
-            request = request.replace('busca en wikipedia', '')
-            wikipedia.set_lang('es')
+            request = request.replace('search wikipedia', '')
+            wikipedia.set_lang('en')
             result = wikipedia.summary(request, sentences=1)
             speak('Wikipedia says the following:')
             speak(result)
             continue
-        elif 'busca en internet' in request:
+        elif 'search internet' in request:
             speak('Searching the internet now')
-            request = request.replace('busca en internet', '')
+            request = request.replace('search internet', '')
             pywhatkit.search(request)
             speak('This is what I found')
             continue
-        elif 'reproducir' in request:
+        elif 'play' in request:
             speak('Good idea, starting playback')
             pywhatkit.playonyt(request)
             continue
-        elif 'broma' in request:
-            speak(pyjokes.get_joke('es'))
+        elif 'joke' in request:
+            speak(pyjokes.get_joke('en'))
             continue
-        elif 'precio de las acciones' in request:
-            stock = request.split('de')[-1].strip()
+        elif 'stock price' in request:
+            stock = request.split('of')[-1].strip()
             portfolio = {'apple':'APPL',
                          'amazon':'AMZN',
                          'google':'GOOGL'}
@@ -220,7 +220,7 @@ def main_loop():
             except:
                 speak("Sorry, I couldn't find it")
                 continue
-        elif 'adiós' in request:
+        elif 'goodbye' in request:
             speak("I'm going to rest, let me know if you need anything")
             break
 
